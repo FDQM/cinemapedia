231c73b53d0b01cb6967f34d16332571862d3125
FDQM <franklindavid.quezadamartinez@gmail.com>
2024-07-14 23:28:07 -0400

    Implementing get movie by id

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
 
 class MovieMapper {
@@ -16,6 +17,26 @@ class MovieMapper {
       posterPath: movieDB.posterPath != ''
           ? 'https://image.tmdb.org/t/p/w500/${movieDB.posterPath}'
           : 'no-poster', //'https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
+      releaseDate: movieDB.releaseDate,
+      title: movieDB.title,
+      video: movieDB.video,
+      voteAverage: movieDB.voteAverage,
+      voteCount: movieDB.voteCount);
+
+      static Movie movieDetailsToEntity(MovieDetails movieDB) => Movie(
+        adult: movieDB.adult,
+      backdropPath: movieDB.backdropPath != ''
+          ? 'https://image.tmdb.org/t/p/w500/${movieDB.backdropPath}'
+          : 'https://img.freepik.com/free-vector/404-error-poster-with-page-found-use-website_1284-49337.jpg',
+      genreIds: movieDB.genres.map((e) => e.name).toList(),
+      id: movieDB.id,
+      originalLanguage: movieDB.originalLanguage,
+      originalTitle: movieDB.originalTitle,
+      overview: movieDB.overview,
+      popularity: movieDB.popularity,
+      posterPath: movieDB.posterPath != ''
+          ? 'https://image.tmdb.org/t/p/w500/${movieDB.posterPath}'
+          : 'https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
       releaseDate: movieDB.releaseDate,
       title: movieDB.title,
       video: movieDB.video,
--- lib/infrastructure/repositories/movie_repository_impl.dart
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -26,5 +26,11 @@ final MoviesDatasource datasource;
   Future<List<Movie>> getUpcoming({int page = 1}) {
     return datasource.getUpcoming(page: page);
   }
+  
+  @override
+  Future<Movie> getMovieById(String id) {
+    return datasource.getMovieById(id);
+  }
+  
 
 }
